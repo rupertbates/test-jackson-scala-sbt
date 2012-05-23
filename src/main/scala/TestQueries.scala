111ce280ecb7096b9e00f88bfc0878eab2f14569
rbates <rupert.bates@guardian.co.uk>
2012-05-23 21:46:33 +0100

    Test some querying stuff

---
+++ src/main/scala/TestQueries.scala
@@ -0,0 +1,8 @@
+import com.mongodb.casbah.commons.MongoDBObject._
+
+object TestQueries {
+  def main(args: Array[String]) {
+    val matches = MatchRepository.find(null).toList
+    val i = 0
+  }
+}
